e503fd0cde15329ae5cd0a7be2410c27a6d1162e
sephiroth-j <sephiroth@sephiroth-j.de>
2019-12-25 15:33:36 +0100

    *new* Support the Reactive Stack with `Ltpa2AuthConverter` and `Ltpa2AuthManager` check the README.md for the details
    issue #1

---
+++ src/main/java/de/sephirothj/spring/security/ltpa2/reactive/Ltpa2AuthConverter.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright 2019 Sephiroth.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.sephirothj.spring.security.ltpa2.reactive;
+
+import de.sephirothj.spring.security.ltpa2.Ltpa2Token;
+import de.sephirothj.spring.security.ltpa2.Ltpa2Utils;
+import java.security.PublicKey;
+import javax.crypto.SecretKey;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
+import org.springframework.util.Assert;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.SynchronousSink;
+
+/**
+ * Strategy for converting a {@link ServerWebExchange} to an {@link Authentication} based on LTPA2 tokens. The final authentication will be done by a {@link Ltpa2AuthManager}. The token is expected to be given in the header {@link #headerName} with an {@link #headerValueIdentifier optional prefix}. If the header is empty or not found the token will be searched in the cookie named {@link #cookieName}.
+ *
+ * @author Sephiroth
+ */
+@Slf4j
+public class Ltpa2AuthConverter implements ServerAuthenticationConverter, InitializingBean
+{
+	/**
+	 * <p>
+	 * the name of the cookie expected to contain the LTPA2 token</p>
+	 * <p>
+	 * default: {@code "LtpaToken2"}</p>
+	 */
+	@Setter
+	@NonNull
+	private String cookieName = "LtpaToken2";
+
+	/**
+	 * <p>
+	 * the name of header expected to contain the LTPA2 token</p>
+	 * <p>
+	 * default: {@value HttpHeaders#AUTHORIZATION}</p>
+	 */
+	@Setter
+	@NonNull
+	private String headerName = HttpHeaders.AUTHORIZATION;
+
+	/**
+	 * <p>
+	 * the prefix in {@link #headername the header} preceding the LTPA2 token</p>
+	 * <p>
+	 * default: {@code cookieName + " "}</p>
+	 *
+	 * @see #cookieName
+	 */
+	@Setter
+	@NonNull
+	private String headerValueIdentifier = cookieName + " ";
+
+	/**
+	 * the public key from the identity provider that sends the LTPA2-tokens. required for signature validation.
+	 */
+	@Setter
+	@NonNull
+	private PublicKey signerKey;
+
+	/**
+	 * the shared secret key that is used to encrypt LTPA2 tokens
+	 */
+	@Setter
+	@NonNull
+	private SecretKey sharedKey;
+
+	/**
+	 * allow expired tokens
+	 * <p>
+	 * <b>Do not use in prodcution mode, only for testing!</b></p>
+	 */
+	@Setter
+	private boolean allowExpiredToken = false;
+
+	@Override
+	public void afterPropertiesSet()
+	{
+		Assert.hasText(cookieName, "A cookieName is required");
+		Assert.hasText(headerName, "A headerName is required");
+		Assert.notNull(headerValueIdentifier, "The headerValueIdentifier must not be null");
+		Assert.notNull(signerKey, "A signerKey is required");
+		Assert.notNull(sharedKey, "A sharedKey is required");
+		if (allowExpiredToken)
+		{
+			log.warn("Expired LTPA2 tokens are allowed, this should only be used for testing!");
+		}
+	}
+
+	private void checkForExpiredToken(final String ltpaToken, final SynchronousSink<String> sink)
+	{
+		try
+		{
+			if (!Ltpa2Utils.isTokenExpired(ltpaToken) || allowExpiredToken)
+			{
+				sink.next(ltpaToken);
+			}
+			else
+			{
+				throw new InsufficientAuthenticationException("token expired");
+			}
+		}
+		catch (AuthenticationException e)
+		{
+			// do not produce mono error, just log and produce empty mono as by contract of ServerAuthenticationConverter
+			log.warn(e.getLocalizedMessage());
+		}
+	}
+
+	private void checkTokenSignature(final String ltpaToken, final SynchronousSink<String> sink)
+	{
+		try
+		{
+			if (Ltpa2Utils.isSignatureValid(ltpaToken, signerKey))
+			{
+				sink.next(ltpaToken);
+			}
+			else
+			{
+				throw new InsufficientAuthenticationException("token signature invalid");
+			}
+		}
+		catch (AuthenticationException e)
+		{
+			// do not produce mono error, just log and produce empty mono as by contract of ServerAuthenticationConverter
+			log.warn(e.getLocalizedMessage());
+		}
+	}
+
+	/**
+	 * Extracts an LTAP2 token from the defined {@link #headerName header} or {@link #cookieName cookie} (as fallback), validates it and if it is, creates an {@link Authentication} instance with it
+	 *
+	 * @param exchange The {@link ServerWebExchange}
+	 * @return A {@link Mono} representing an {@link Authentication} with a valid {@link Ltpa2Token} as credentials or an empty Mono if the token was not found or is invalid
+	 */
+	@Override
+	public Mono<Authentication> convert(ServerWebExchange exchange)
+	{
+		final ServerHttpRequest request = exchange.getRequest();
+		return Mono.justOrEmpty(request.getHeaders().getFirst(headerName))
+			.filter(header -> !header.isEmpty() && header.startsWith(headerValueIdentifier))
+			.map(header -> header.substring(header.indexOf(headerValueIdentifier) + headerValueIdentifier.length()))
+			// try cookie as fallback
+			.switchIfEmpty(Mono.defer(() -> Mono.justOrEmpty(request.getCookies().getFirst(cookieName)).map(HttpCookie::getValue)))
+			.doOnNext(encryptedToken -> log.debug("raw LTPA2 token: {}", encryptedToken))
+			.map(encryptedToken -> Ltpa2Utils.decryptLtpa2Token(encryptedToken, sharedKey))
+			.onErrorResume(e ->
+			{
+				log.warn(e.getLocalizedMessage());
+				return Mono.empty();
+			})
+			.handle(this::checkForExpiredToken)
+			.handle(this::checkTokenSignature)
+			.map(Ltpa2Utils::makeInstance)
+			.map(token -> new PreAuthenticatedAuthenticationToken(token.getUser(), token));
+	}
+
+}
---
+++ src/test/java/de/sephirothj/spring/security/ltpa2/reactive/Ltpa2AuthConverterTest.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2019 Sephiroth.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.sephirothj.spring.security.ltpa2.reactive;
+
+import de.sephirothj.spring.security.ltpa2.Constants;
+import de.sephirothj.spring.security.ltpa2.Ltpa2Token;
+import de.sephirothj.spring.security.ltpa2.LtpaKeyUtils;
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.HttpHeaders;
+import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
+import org.springframework.mock.web.server.MockServerWebExchange;
+import org.springframework.security.core.Authentication;
+import org.springframework.util.Base64Utils;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ *
+ * @author Sephiroth
+ */
+class Ltpa2AuthConverterTest
+{
+
+	@Test
+	void getTokenFromHeaderTestWithDefaultPrefix() throws GeneralSecurityException
+	{
+		Ltpa2AuthConverter uut = new Ltpa2AuthConverter();
+		uut.setSharedKey(LtpaKeyUtils.decryptSharedKey(Constants.ENCRYPTED_SHARED_KEY, Constants.ENCRYPTION_PASSWORD));
+		uut.setSignerKey(LtpaKeyUtils.decodePublicKey(Constants.ENCODED_PUBLIC_KEY));
+		uut.setAllowExpiredToken(true);
+		uut.afterPropertiesSet();
+
+		MockServerHttpRequest.BaseBuilder requestBuilder = MockServerHttpRequest.get("/").header(HttpHeaders.AUTHORIZATION, "LtpaToken2 ".concat(Constants.TEST_TOKEN));
+		StepVerifier.create(uut.convert(MockServerWebExchange.from(requestBuilder.build())))
+			.assertNext(this::verifyAuth)
+			.verifyComplete();
+	}
+
+	@Test
+	void getTokenFromHeaderTestWithCustomPrefix() throws GeneralSecurityException
+	{
+		Ltpa2AuthConverter uut = new Ltpa2AuthConverter();
+		uut.setSharedKey(LtpaKeyUtils.decryptSharedKey(Constants.ENCRYPTED_SHARED_KEY, Constants.ENCRYPTION_PASSWORD));
+		uut.setSignerKey(LtpaKeyUtils.decodePublicKey(Constants.ENCODED_PUBLIC_KEY));
+		uut.setAllowExpiredToken(true);
+		String prefix = "my-prefix";
+		uut.setHeaderValueIdentifier(prefix);
+
+		MockServerHttpRequest.BaseBuilder requestBuilder = MockServerHttpRequest.get("/").header(HttpHeaders.AUTHORIZATION, prefix.concat(Constants.TEST_TOKEN));
+		StepVerifier.create(uut.convert(MockServerWebExchange.from(requestBuilder.build())))
+			.assertNext(this::verifyAuth)
+			.verifyComplete();
+	}
+
+	@Test
+	void getTokenFromHeaderTestWithEmptyPrefix() throws GeneralSecurityException
+	{
+		Ltpa2AuthConverter uut = new Ltpa2AuthConverter();
+		uut.setSharedKey(LtpaKeyUtils.decryptSharedKey(Constants.ENCRYPTED_SHARED_KEY, Constants.ENCRYPTION_PASSWORD));
+		uut.setSignerKey(LtpaKeyUtils.decodePublicKey(Constants.ENCODED_PUBLIC_KEY));
+		uut.setAllowExpiredToken(true);
+		uut.setHeaderValueIdentifier("");
+
+		MockServerHttpRequest.BaseBuilder requestBuilder = MockServerHttpRequest.get("/").header(HttpHeaders.AUTHORIZATION, Constants.TEST_TOKEN);
+		StepVerifier.create(uut.convert(MockServerWebExchange.from(requestBuilder.build())))
+			.assertNext(this::verifyAuth)
+			.verifyComplete();
+	}
+
+	@Test
+	void getTokenFromHeaderTestWithCustomName() throws GeneralSecurityException
+	{
+		Ltpa2AuthConverter uut = new Ltpa2AuthConverter();
+		uut.setSharedKey(LtpaKeyUtils.decryptSharedKey(Constants.ENCRYPTED_SHARED_KEY, Constants.ENCRYPTION_PASSWORD));
+		uut.setSignerKey(LtpaKeyUtils.decodePublicKey(Constants.ENCODED_PUBLIC_KEY));
+		uut.setAllowExpiredToken(true);
+		String headerName = "my-header";
+		uut.setHeaderName(headerName);
+
+		MockServerHttpRequest.BaseBuilder requestBuilder = MockServerHttpRequest.get("/").header(headerName, "LtpaToken2 ".concat(Constants.TEST_TOKEN));
+		StepVerifier.create(uut.convert(MockServerWebExchange.from(requestBuilder.build())))
+			.assertNext(this::verifyAuth)
+			.verifyComplete();
+	}
+
+	@Test
+	void getTokenFromCookieTestWithDefaultName() throws GeneralSecurityException
+	{
+		Ltpa2AuthConverter uut = new Ltpa2AuthConverter();
+		uut.setSharedKey(LtpaKeyUtils.decryptSharedKey(Constants.ENCRYPTED_SHARED_KEY, Constants.ENCRYPTION_PASSWORD));
+		uut.setSignerKey(LtpaKeyUtils.decodePublicKey(Constants.ENCODED_PUBLIC_KEY));
+		uut.setAllowExpiredToken(true);
+
+		MockServerHttpRequest.BaseBuilder requestBuilder = MockServerHttpRequest.get("/").cookie(new HttpCookie("LtpaToken2", Constants.TEST_TOKEN));
+		StepVerifier.create(uut.convert(MockServerWebExchange.from(requestBuilder.build())))
+			.assertNext(this::verifyAuth)
+			.verifyComplete();
+	}
+
+	@Test
+	void getTokenFromCookieTestWithCustomName() throws GeneralSecurityException
+	{
+		Ltpa2AuthConverter uut = new Ltpa2AuthConverter();
+		uut.setSharedKey(LtpaKeyUtils.decryptSharedKey(Constants.ENCRYPTED_SHARED_KEY, Constants.ENCRYPTION_PASSWORD));
+		uut.setSignerKey(LtpaKeyUtils.decodePublicKey(Constants.ENCODED_PUBLIC_KEY));
+		uut.setAllowExpiredToken(true);
+		String cookieName = "my-cookie";
+		uut.setCookieName(cookieName);
+
+		MockServerHttpRequest.BaseBuilder requestBuilder = MockServerHttpRequest.get("/").cookie(new HttpCookie(cookieName, Constants.TEST_TOKEN));
+		StepVerifier.create(uut.convert(MockServerWebExchange.from(requestBuilder.build())))
+			.assertNext(this::verifyAuth)
+			.verifyComplete();
+	}
+
+	@Test
+	void expiredTokenShouldBeRejected() throws GeneralSecurityException
+	{
+		Ltpa2AuthConverter uut = new Ltpa2AuthConverter();
+		uut.setSharedKey(LtpaKeyUtils.decryptSharedKey(Constants.ENCRYPTED_SHARED_KEY, Constants.ENCRYPTION_PASSWORD));
+		uut.setSignerKey(LtpaKeyUtils.decodePublicKey(Constants.ENCODED_PUBLIC_KEY));
+
+		MockServerHttpRequest.BaseBuilder requestBuilder = MockServerHttpRequest.get("/").header(HttpHeaders.AUTHORIZATION, "LtpaToken2 ".concat(Constants.TEST_TOKEN));
+		StepVerifier.create(uut.convert(MockServerWebExchange.from(requestBuilder.build())))
+			.verifyComplete();
+	}
+
+	@Test
+	void invalidSignaturesShouldBeRejected() throws GeneralSecurityException
+	{
+		Ltpa2AuthConverter uut = new Ltpa2AuthConverter();
+		final SecretKey decryptSharedKey = LtpaKeyUtils.decryptSharedKey(Constants.ENCRYPTED_SHARED_KEY, Constants.ENCRYPTION_PASSWORD);
+		uut.setSharedKey(decryptSharedKey);
+		uut.setSignerKey(LtpaKeyUtils.decodePublicKey(Constants.ENCODED_PUBLIC_KEY));
+		uut.setAllowExpiredToken(true);
+
+		String tokenWithInvalidSignature = "expire:1519043460000$u:user\\:LdapRegistry/CN=fae6d87c-c642-45a6-9f09-915c7fd8b08c,OU=user,DC=foo,DC=bar%1519043460000%ipDldknyTbaSZluHTW3I/Dhh9veyi+QHoX3s4MPxvvTc09COCGGbOQLxiGoIqdBxDrv55WChFNDD6uUtnt74gNX2KTRQpbwY5zSMbNHkUrh/6X+OOqbvcR3fAmIBkTAyBwkX3u6T2WEoEq9FxOYpvlhqvygoJYrjM6JuQeGhvBB=";
+		final Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		final IvParameterSpec iv = new IvParameterSpec(decryptSharedKey.getEncoded());
+		c.init(Cipher.ENCRYPT_MODE, decryptSharedKey, iv);
+		final byte[] rawEncryptedToken = c.doFinal(tokenWithInvalidSignature.getBytes(StandardCharsets.UTF_8));
+		String encryptedToken = Base64Utils.encodeToString(rawEncryptedToken);
+
+		MockServerHttpRequest.BaseBuilder requestBuilder = MockServerHttpRequest.get("/").header(HttpHeaders.AUTHORIZATION, "LtpaToken2 ".concat(encryptedToken));
+		StepVerifier.create(uut.convert(MockServerWebExchange.from(requestBuilder.build())))
+			.verifyComplete();
+	}
+
+	@Test
+	void invalidTokensShouldBeRejected() throws GeneralSecurityException
+	{
+		Ltpa2AuthConverter uut = new Ltpa2AuthConverter();
+		uut.setSharedKey(LtpaKeyUtils.decryptSharedKey(Constants.ENCRYPTED_SHARED_KEY, Constants.ENCRYPTION_PASSWORD));
+		uut.setSignerKey(LtpaKeyUtils.decodePublicKey(Constants.ENCODED_PUBLIC_KEY));
+
+		MockServerHttpRequest.BaseBuilder requestBuilder = MockServerHttpRequest.get("/").header(HttpHeaders.AUTHORIZATION, "LtpaToken2 asd");
+		StepVerifier.create(uut.convert(MockServerWebExchange.from(requestBuilder.build())))
+			.verifyComplete();
+	}
+
+	@Test
+	void malformedTokensShouldBeRejected() throws GeneralSecurityException
+	{
+		Ltpa2AuthConverter uut = new Ltpa2AuthConverter();
+		final SecretKey decryptSharedKey = LtpaKeyUtils.decryptSharedKey(Constants.ENCRYPTED_SHARED_KEY, Constants.ENCRYPTION_PASSWORD);
+		uut.setSharedKey(decryptSharedKey);
+		uut.setSignerKey(LtpaKeyUtils.decodePublicKey(Constants.ENCODED_PUBLIC_KEY));
+		uut.setAllowExpiredToken(true);
+
+		String tokenWithMissingSignature = "expire:1519043460000$u:user\\:LdapRegistry/CN=fae6d87c-c642-45a6-9f09-915c7fd8b08c,OU=user,DC=foo,DC=bar%1519043460000";
+		final Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		final IvParameterSpec iv = new IvParameterSpec(decryptSharedKey.getEncoded());
+		c.init(Cipher.ENCRYPT_MODE, decryptSharedKey, iv);
+		final byte[] rawEncryptedToken = c.doFinal(tokenWithMissingSignature.getBytes(StandardCharsets.UTF_8));
+		String encryptedToken = Base64Utils.encodeToString(rawEncryptedToken);
+
+		MockServerHttpRequest.BaseBuilder requestBuilder = MockServerHttpRequest.get("/").header(HttpHeaders.AUTHORIZATION, "LtpaToken2 ".concat(encryptedToken));
+		StepVerifier.create(uut.convert(MockServerWebExchange.from(requestBuilder.build())))
+			.verifyComplete();
+	}
+
+	private void verifyAuth(Authentication auth)
+	{
+		assertThat(auth.isAuthenticated()).isFalse();
+		assertThat(auth.getCredentials()).isInstanceOf(Ltpa2Token.class);
+		assertThat(auth.getPrincipal()).isEqualTo(auth.getName()).asString().isEqualTo("user:LdapRegistry/CN=fae6d87c-c642-45a6-9f09-915c7fd8b08c,OU=user,DC=foo,DC=bar");
+	}
+}
